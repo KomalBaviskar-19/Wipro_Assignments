1470c9e4028a963fb0edf83aecbe7e1fa3a67635
Komal_Baviskar <komalbaviskar19@gmail.com>
2025-08-17 00:56:09 +0530

    Add files via upload

---
+++ Collection_Assignments/src/com/wipro/komal/GroceryLineShuffle.java
@@ -0,0 +1,23 @@
+package com.wipro.komal;
+
+import java.util.ArrayDeque;
+
+
+public class GroceryLineShuffle {
+	public static void main(String[] args) {
+        ArrayDeque<String> line = new ArrayDeque<>();
+        addCustomer(line, "Ravi");
+        addCustomer(line, "Meena");
+        addCustomer(line, "John");
+        addCustomer(line, "Alex");
+
+        System.out.println("Grocery Queue: " + line);
+    }
+
+    static void addCustomer(ArrayDeque<String> line, String name) {
+        if (name.length() % 2 == 0)
+            line.addFirst(name);
+        else
+            line.addLast(name);
+    }
+}
